67a9f4be1b5d6e8fd2bcb1e1b9454096623c7c33
ameyanaik <ameya.naik@gmail.com>
2024-09-29 13:18:50 +0530

    Made Changes to POM Selenium Base

--- SJSeleniumWithMavenTestNGPOMSep24/src/test/java/org/skilljourney/trainings/pom/common/POMSeleniumBase.java
+++ SJSeleniumWithMavenTestNGPOMSep24/src/test/java/org/skilljourney/trainings/pom/common/POMSeleniumBase.java
@@ -1,5 +1,7 @@
 package org.skilljourney.trainings.pom.common;
 
+import java.time.Duration;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -9,6 +11,7 @@ public class POMSeleniumBase {
 	
 	public void openBrowser() {
 		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 	}
 
 }
